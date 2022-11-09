763403827f4965175067891d37c1b9f041232984
eldenhard <elden.hard@mail.ru>
2022-11-09 17:23:12 +0300

    калькулятор

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
 // import { createApp } from './vendor/vue.esm-browser.js';
 
+import { createApp } from "../01-CounterButtonApp/vendor/vue.esm-browser";
+
 // Создайте Vue приложение
+let vm = createApp({
+    data() {
+        return {
+            num1: 0,
+            num2: 0,
+            operator: ''
+        }
+    },
+    computed: {
+        result() {
+            switch(this.operator) {
+              case "sum":
+                return this.num1 + this.num2
+              case "subtract":
+                return this.num1 - this.num2
+              case "multiply":
+                return this.num1 * this.num2
+              case "divide":
+                return this.num1 / this.num2
+            }
+          }
+    }
+}).mount('#app')
