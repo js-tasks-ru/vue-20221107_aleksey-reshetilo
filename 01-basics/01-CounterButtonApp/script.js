d70b0c061a426313f80af853fab8b107a295c1e3
eldenhard <elden.hard@mail.ru>
2022-11-09 14:20:30 +0300

    counter-task

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,11 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const Task1 = createApp({
+    data() {
+        return {
+            counter: 0
+        }
+    }
+}).mount('#app')
