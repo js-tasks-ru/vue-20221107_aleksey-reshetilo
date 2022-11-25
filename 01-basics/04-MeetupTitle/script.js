92e7c8faa044c6b5b35af2d6bab862c93735221d
eldenhard <elden.hard@mail.ru>
2022-11-26 01:28:38 +0300

    задание 4 2 раздел

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,11 +1,11 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
 function fetchMeetupById(meetupId) {
   return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
     if (response.ok) {
-      return response.json();
+      return this.result = response.json();
     } else {
       return response.json().then((error) => {
         throw error;
@@ -13,5 +13,21 @@ function fetchMeetupById(meetupId) {
     }
   });
 }
+
+
+let vm = createApp({
+  data: () => ({
+    meetupId: null,
+    meetupData: null,
+  }),
+  watch: {
+    meetupId(val) {
+      fetch(`https://course-vue.javascript.ru/api/meetups/${val}`)
+        .then(response => response.json())
+        .then(response => this.meetupData = response.title);
+    }
+  },
+
+}).mount('#app')
 
 // Требуется создать Vue приложение
