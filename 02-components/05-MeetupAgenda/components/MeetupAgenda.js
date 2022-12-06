d69a6124d64cb0c4d8ff62aae6209e0d516ab1fc
eldenhard <elden.hard@mail.ru>
2022-12-06 22:23:06 +0300

    qwe

---
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -0,0 +1,20 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
+
+export default defineComponent({
+  name: 'MeetupAgenda',
+  components: { MeetupAgendaItem },
+  props: {
+    agenda: {
+      type: Array
+    }
+  },
+  template: `
+    <ul class="agenda">
+      <li class="agenda__item">
+        <!-- {{agenda}} -->
+        <!-- meetup agenda item -->
+        <!-- <MeetupAgendaItem /> -->
+      </li>
+    </ul>`,
+});
