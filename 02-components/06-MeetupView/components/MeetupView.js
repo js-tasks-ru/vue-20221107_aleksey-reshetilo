ec48a74f5fc4a93880cf2b97d25e8c80b2eddb09
eldenhard <elden.hard@mail.ru>
2022-12-13 16:33:10 +0300

    задачи

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,4 +1,8 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
+import MeetupCover from 'C:/Users/Asus/Desktop/course-vue/vue-20221107_aleksey-reshetilo/02-components/03-MeetupCover/components/MeetupCover.js'
+import MeetupDescription from 'C:/Users/Asus/Desktop/course-vue/vue-20221107_aleksey-reshetilo/02-components/02-MeetupDescription/components/MeetupDescription.js'
+import MeetupAgenda from 'C:/Users/Asus/Desktop/course-vue/vue-20221107_aleksey-reshetilo/02-components/05-MeetupAgenda/components/MeetupAgenda.js'
+import MeetupInfo from 'C:/Users/Asus/Desktop/course-vue/vue-20221107_aleksey-reshetilo/02-components/04-MeetupInfo/components/MeetupInfo.js'
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
 
@@ -8,6 +12,10 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupDescription,
+    MeetupCover,
+    MeetupAgenda,
+    MeetupInfo,
   },
 
   props: {
@@ -20,18 +28,21 @@ export default defineComponent({
   template: `
     <div>
       <!-- meetup cover-->
-
+    <MeetupCover :title="meetup.title" :image="meetup.image" />
       <ui-container>
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
             <!-- meetup description -->
+            <MeetupDescription :description="meetup.description"/>
 
             <h3>Программа</h3>
             <!-- meetup agenda -->
-            <ui-alert>Программа пока пуста...</ui-alert>
+             <MeetupAgenda :agenda="meetup.agenda" v-if="meetup.agenda.length != 0"/>
+            <ui-alert v-if="meetup.agenda.length == 0" >Программа пока пуста...</ui-alert>
           </div>
           <div class="meetup__aside">
+            <MeetupInfo :organizer="meetup.organizer" :place="meetup.place" :date="meetup.date"/>
             <!-- meetup info -->
           </div>
         </div>
